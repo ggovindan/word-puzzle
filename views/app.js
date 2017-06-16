ce0bfdf9ac77cf55368fd169d6546c6d6e52dc5e
ggovindan <gxguru@gmail.com>
2017-06-15 23:08:42 -0700

    Changes to support highlighting answers

--- views/app.js
+++ views/app.js
@@ -1,11 +1,22 @@
 // app.js
 
+var directions = {
+  horizontal: (x,y, i) => { return {x: x+i, y: y}},
+  vertical: (x,y,i) => {return {x: x, y: y+i}},
+  verticalUp: (x,y,i) => {return {x: x, y: y-i}},
+  diagonal: (x,y,i) => {return {x: x+i, y: y+i}},
+  diagonalBack: (x,y,i) => {return {x: x-1, y: y+i}},
+  diagonalUp: (x,y,i) => {return {x: x+i, y: y-i}},
+
+};
+
 new Vue({
   el: "#wordPuzzle",
 
   data: {
           puzzle: [],
-          wordsList: []
+          wordsList: [],
+          solution: []
         },
 
   mounted: function() {
@@ -17,11 +28,34 @@ new Vue({
       this.$http.get('/newPuzzle')
         .then((result) => {
           console.log('result=', result);
-          this.$set(this, 'puzzle', result.data.puzzle);
+          this.$set(this, 'puzzle', result.data.result.puzzle);
           this.$set(this, 'wordsList', result.data.wordsList);
+          this.$set(this, 'solution', result.data.result.solution);
         }, (err) => {
           console.log('error=', err);
         });
+    },
+    check: function(row, col) {
+      console.log("row=", row);
+      console.log("col=", col);
+    },
+    solve: function() {
+      for (var i = 0; i < this.solution.length; i++) {
+        var word = this.solution[i].word;
+        var orientation = this.solution[i].location;
+        var x = orientation.x, y = orientation.y;
+        var next = directions[orientation.direction];
+        console.log('direction=', orientation.direction);
+
+        for (var j = 0; j < word.length; j++) {
+          var nextPos = next(x, y, j);
+          console.log('nextPos=', nextPos);
+          var myId = "row=" + nextPos.y + "col=" + nextPos.x;
+          console.log('calculated_id=', myId);
+          var box = document.getElementById(myId);
+          box.classList.add('solved0');
+        }
+      }
     }
   }
 });
